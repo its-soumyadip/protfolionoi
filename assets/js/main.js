c4c60e6a030822e31b3ed243af0366d8d5205385
Killer-programmer <65546124+Killer-programmer@users.noreply.github.com>
2021-06-08 23:27:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(10).fadeOut('slow', function() {
         $(this).remove();
       });
     }
